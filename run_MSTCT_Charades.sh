4b5be49768b8b18007e508952f36635654ee68b8
areka <areka@nefgpu46.inria.fr>
2025-02-25 14:45:09 +0100

    first

---
+++ run_MSTCT_Charades.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+export PATH=/pytorch_env/bin:$PATH
+
+python train.py \
+-dataset charades \
+-mode rgb \
+-model MS_TCT \
+-train True \
+-num_clips 78 \
+-skip 4 \
+-lr 0.00001 \
+-comp_info False \
+-epoch 30 \
+-unisize True \
+-alpha_l 10 \
+-beta_l 1 \
+-batch_size 1 \
+-num_layer 8
